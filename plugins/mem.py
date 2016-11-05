68c42986d5df8803c9c13730119d4b2664dc3039
Lich2013 <longzy2013@gmail.com>
2016-11-05 20:29:09 +0800

    swap ok

--- plugins/mem.py
+++ plugins/mem.py
@@ -4,9 +4,16 @@ import psutil
 
 class Mem():
     def __init__(self):
-        self.mem = psutil.virtual_memory # 字节
+        self.mem = psutil.virtual_memory  # 字节
         self.memSize = self.mem().total
         self.memFree = self.mem().free
-        self.memAvailable = self.mem().available # windows中free == available
+        self.memAvailable = self.mem().available  # windows中free == available
         self.memUsed = self.mem().used
         self.memPercent = self.mem().percent
+        self.swap = psutil.swap_memory
+        self.swapSize = self.swap().total
+        self.swapFree = self.swap().free
+        self.swapUsed = self.swap().used
+        self.swapPercent = self.swap().percent
+        self.swapInput = self.swap().sin
+        self.swapOutput = self.swap().sout
