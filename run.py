4d22dc4b0f73b21f890930767b16a346ed58393c
Lich2013 <longzy2013@gmail.com>
2017-10-19 00:49:44 +0800

    zlb

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
-# encoding: utf-8
-from daemon import runner
-from common.AgentDaemon import AgentDaemon
+# encoding:utf-8
+import sys
+from common.Daemon import Daemon
 
 if __name__ == "__main__":
-    d = AgentDaemon()
-    dd = runner.DaemonRunner(d)
-    dd.do_action()
+    daemon = Daemon()
+    if len(sys.argv) >= 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
